e41f5d02dcef3153466ceaedd829fbb26b29b7d5
francitonioferreiraalves <francitonio@gmail.com>
2021-07-31 15:13:19 -0300

    first commit

---
+++ src/main/java/com/tito/springbootcriptografarsenha/SpringBootCriptografarSenhaApplication.java
@@ -0,0 +1,13 @@
+package com.tito.springbootcriptografarsenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCriptografarSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCriptografarSenhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tito/springbootcriptografarsenha/SpringBootCriptografarSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tito.springbootcriptografarsenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCriptografarSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
